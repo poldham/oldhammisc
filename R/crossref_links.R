e9b5f1bc62a1b0082ba9b5159d9fa1f74118fca8
Paul Oldham <poldham@mac.com>
2016-06-13 15:10:51 +0100

    I'll try that again..

--- R/crossref_links.R
+++ R/crossref_links.R
@@ -1,17 +1,16 @@
-#' @title link
-#' @description  
-#' @param data 
-#' @param col 
-#' @param sep 
-#' @param convert 
-#'
-#' @return
+#' @title Extract crossref link data into a data.frame
+#' @description  Takes the return from rcrossref \code{cr_works} and extracts the data in the link field and joins it to the source data.frame.
+#' @param col the list column of a data.frame containing cross ref link
+#' @param source The source data.frame
+#' @return A data.frame
+#' @details A DOI (quoted) column must be present in the source data.frame.
 #' @export
-#'
-#' @examples
+#' @importFrom plyr ldply
+#' @references \url{https://github.com/ropensci/rcrossref}
+#' @examples \dontrun{links(col, source)}
 links <- function(col, source){
-  data <- plyr::ldply(synbio_crossref$link, .id = "DOI") %>% 
+  data <- plyr::ldply(synbio_crossref$link, .id = "DOI") %>%
     rename(intended_application = intended.application, content_version = content.version, content_type = content.type, link_URL = URL)
-  data <- dplyr::full_join(data, source, by = as.character("DOI"))
-  
+  data <- dplyr::left_join(data, source, by = as.character("DOI"))
+
 }
--- R/crossref_rename.R
+++ R/crossref_rename.R
@@ -1,8 +1,17 @@
+#' @title Rename crossref fields
+#' @description Start of a function to rename fields generated from rcrossref \code{cr_works} with underscores. Does not yet address complete field names.
+#' @param x A data.frame with columns to rename
+#'
+#' @return A data.frame
+#' @export
+#' @importFrom dplyr rename
+#' @references \url{https://github.com/ropensci/rcrossref}
+#' @examples \dontrun{crossref_rename}
 crossref_rename <- function(x){
-  dplyr::rename(x, alternative_id = alternative.id, 
+  dplyr::rename(x, alternative_id = alternative.id,
   container_title = container.title,
   reference_count = reference.count,
   license_content_version = license_content.version,
-  license_delay_in_days = license_delay.in.days, 
-  update_policy = update.policy)          
+  license_delay_in_days = license_delay.in.days,
+  update_policy = update.policy)
 }
