b3ebd082d765f267e1bda1847a5e655ca63ee5f2
Paul Oldham <poldham@mac.com>
2016-07-30 11:08:03 +0100

    Revert "Adding text functions for plot and crossref"
    This reverts commit 8da2df99d443beaa2927157972aab112a7abfa54.

--- R/plos_results.R
+++
@@ -1,7 +0,0 @@
-plos_results <- function(query = "", limit = ""){
-  df <- lapply(s, function(x) searchplos(x, fl = c('id', 'author', 'publication_date', 'title', 'abstract'), limit = as.numeric(limit)) %>%
-      setNames(s) %>% 
-      ldply("[[", 2)
-}
-
-  
